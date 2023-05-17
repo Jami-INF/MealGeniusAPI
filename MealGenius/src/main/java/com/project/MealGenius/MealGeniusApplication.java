b01c36ca75838a134c6c1020bde6f3f6a838efd6
Jami infante <jami@MacBook.local>
2023-05-17 16:49:06 +0200

    🎉 first commit

---
+++ MealGenius/src/main/java/com/project/MealGenius/MealGeniusApplication.java
@@ -0,0 +1,13 @@
+package com.project.MealGenius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealGeniusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealGeniusApplication.class, args);
+	}
+
+}
