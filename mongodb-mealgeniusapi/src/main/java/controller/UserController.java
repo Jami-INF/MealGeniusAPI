5df4d08e9248db533e1a245e137488400f6a9495
Jami infante <jami@MacBook.local>
2023-05-31 08:39:30 +0200

    ADD : controller + DTO + Model + Mapper + Repository

--- mongodb-mealgeniusapi/src/main/java/controller/UserController.java
+++ mongodb-mealgeniusapi/src/main/java/controller/UserController.java
@@ -1,44 +1,53 @@
 package controller;
 
+import dto.UserDTO;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import mapper.UserMapper;
 import model.User;
 import org.bson.types.ObjectId;
 import repository.UserRepository;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Path("/api/users")
 public class UserController {
+
     private final UserRepository userRepository;
+    private UserMapper userMapper;
 
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.userMapper = new UserMapper();
     }
 
     @GET
     public List<User> getUsers() {
-        return userRepository.listAll();
+        List<User> users = new ArrayList<User>();
+        users.addAll(userRepository.listAll());
+        return users;
     }
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public User getUser (@PathParam("id") String id) {
-        return userRepository.findById(id);
+    public UserDTO getUser (@PathParam("id") String id) {
+        return UserMapper.userToUserDTO(userRepository.findById(id));
     }
 
     @PUT
     @Path("/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public void updateUser (@PathParam("id") String id, User user) {
+    public void updateUser (@PathParam("id") String id, UserDTO user) {
         user.setId(new ObjectId(id));
-        userRepository.update(user);
+        userRepository.update(UserMapper.userDTOToUser(user));
     }
     @POST
-    public Response addUser(User user) {
+    public Response addUser(UserDTO userDTO) {
+        User user = UserMapper.userDTOToUser(userDTO);
         userRepository.persist(user);
         return Response.status(Response.Status.CREATED).build();
     }
@@ -49,4 +58,11 @@ public class UserController {
         User user = userRepository.findById(id);
         userRepository.delete(user);
     }
+
+    @POST
+    @Path("/initdb")
+    public void initDB() {
+        userRepository.initDB();
+    }
+
 }
---
+++ mongodb-mealgeniusapi/src/main/java/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package mapper;
+
+import dto.UserDTO;
+import jakarta.enterprise.context.ApplicationScoped;
+import model.User;
+
+@ApplicationScoped
+public class UserMapper {
+
+    public static UserDTO userToUserDTO(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setFirstName(user.getFirstName());
+        userDTO.setLastName(user.getLastName());
+        userDTO.setEmail(user.getEmail());
+        return userDTO;
+    }
+
+    public static User userDTOToUser(UserDTO userDTO) {
+        User user = new User();
+        user.setId(userDTO.getId());
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        user.setEmail(userDTO.getEmail());
+        return user;
+    }
+}
