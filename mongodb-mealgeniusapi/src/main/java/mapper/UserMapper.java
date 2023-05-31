8b37f03d1aafd091918192943833a94cfd603b16
Jami infante <jami@MacBook.local>
2023-05-31 11:56:13 +0200

    UPD : entity User

--- mongodb-mealgeniusapi/src/main/java/mapper/UserMapper.java
+++ mongodb-mealgeniusapi/src/main/java/mapper/UserMapper.java
@@ -2,26 +2,49 @@ package mapper;
 
 import dto.UserDTO;
 import jakarta.enterprise.context.ApplicationScoped;
-import model.User;
+import entity.UserEntity;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
 
 @ApplicationScoped
 public class UserMapper {
 
-    public static UserDTO userToUserDTO(User user) {
+    public static UserDTO userToUserDTO(UserEntity userEntity) {
         UserDTO userDTO = new UserDTO();
-        userDTO.setId(user.getId());
-        userDTO.setFirstName(user.getFirstName());
-        userDTO.setLastName(user.getLastName());
-        userDTO.setEmail(user.getEmail());
+        userDTO.setId(userEntity.getId());
+        userDTO.setFirstName(userEntity.getFirstName());
+        userDTO.setLastName(userEntity.getLastName());
+        userDTO.setEmail(userEntity.getEmail());
         return userDTO;
     }
 
-    public static User userDTOToUser(UserDTO userDTO) {
-        User user = new User();
-        user.setId(userDTO.getId());
-        user.setFirstName(userDTO.getFirstName());
-        user.setLastName(userDTO.getLastName());
-        user.setEmail(userDTO.getEmail());
-        return user;
+    public static UserEntity userDTOToUser(UserDTO userDTO) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(userDTO.getId());
+        userEntity.setFirstName(userDTO.getFirstName());
+        userEntity.setLastName(userDTO.getLastName());
+        userEntity.setEmail(userDTO.getEmail());
+        return userEntity;
+    }
+
+    public static Document userToDocument(UserEntity userEntity) {
+        Document doc = new Document()
+                .append("_id", new ObjectId(userEntity.getId()))
+                .append("firstname", userEntity.getFirstName())
+                .append("lastname", userEntity.getLastName())
+                .append("email", userEntity.getEmail())
+                .append("password", userEntity.getPassword());
+        return doc;
+    }
+
+    public UserEntity documentToUser(Document doc){
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(doc.getObjectId("_id").toHexString());
+        userEntity.setFirstName(doc.getString("firstname"));
+        userEntity.setLastName(doc.getString("lastname"));
+        userEntity.setEmail(doc.getString("email"));
+        userEntity.setPassword(doc.getString("password"));
+        return userEntity;
     }
 }
--- mongodb-mealgeniusapi/src/main/java/repository/UserRepository.java
+++ mongodb-mealgeniusapi/src/main/java/repository/UserRepository.java
@@ -1,12 +1,11 @@
 package repository;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import model.User;
+import mapper.UserMapper;
+import entity.UserEntity;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Default;
 import jakarta.inject.Inject;
@@ -20,9 +19,11 @@ import java.util.List;
 @Default
 public class UserRepository {
 
-    private static final String DATABASE_NAME = "mealgenius";
     @Inject
     MongoClient mongoClient;
+    private static final String DATABASE_NAME = "mealgenius";
+
+    private UserMapper userMapper = new UserMapper();
 
     private MongoCollection<Document> collection = getConnexion().getCollection("users");;
 
@@ -30,68 +31,48 @@ public class UserRepository {
         mongoClient.getDatabase("test").getCollection("users");
     }
 
-    public void persist(User user) {
-        Document doc = new Document()
-                .append("firstname", user.getFirstName())
-                .append("lastname", user.getLastName())
-                .append("email", user.getEmail())
-                .append("password", user.getPassword());
-        collection.insertOne(doc);
-        user.setId(doc.getObjectId("_id"));
+    public void addUser(UserEntity userEntity) {
+        Document userDoc = UserMapper.userToDocument(userEntity);
+        collection.insertOne(userDoc);
     }
 
-    public User findById(String id) {
+    public UserEntity findById(String id) {
         Document query = new Document("_id", new ObjectId(id));
         Document userDoc = collection.find(query).first();
         if (userDoc != null) {
-            return documentToUser(userDoc);
+            return userMapper.documentToUser(userDoc);
         }
         return null;
     }
 
-    public User findByUsername(String username) {
+    public UserEntity findByUsername(String username) {
         Document query = new Document("username", username);
         Document userDoc = collection.find(query).first();
         if (userDoc != null) {
-            return documentToUser(userDoc);
+            return userMapper.documentToUser(userDoc);
         }
         return null;
     }
 
-    public void delete(User user) {
-        Document query = new Document("_id", user.getId());
+    public void delete(UserEntity userEntity) {
+        Document query = new Document("_id", userEntity.getId());
         collection.deleteOne(query);
     }
 
-    public void update(User user) {
-        Document query = new Document("_id", user.getId());
-        Document doc = new Document()
-                .append("firstname", user.getFirstName())
-                .append("lastname", user.getLastName())
-                .append("email", user.getEmail())
-                .append("password", user.getPassword());
-        collection.updateOne(query, new Document("$set", doc));
+    public void update(UserEntity userEntity) {
+        Document query = new Document("_id", userEntity.getId());
+        collection.updateOne(query, new Document("$set", UserMapper.userToDocument(userEntity)));
     }
 
-    public List<User> listAll() {
+    public List<UserEntity> listAll() {
         List<Document> documents = collection.find().into(new ArrayList<>());
-        List<User> users = new ArrayList<>();
+        List<UserEntity> userEntities = new ArrayList<>();
         for (Document document : documents) {
-            users.add(documentToUser(document));
+            userEntities.add(userMapper.documentToUser(document));
         }
-        return users;
+        return userEntities;
     }
 
-    private User documentToUser(Document document) {
-        User user = new User(
-                document.getObjectId("_id"),
-                document.getString("firstname"),
-                document.getString("lastname"),
-                document.getString("email"),
-                document.getString("password")
-        );
-        return user;
-    }
 
     public void initDB(){
         //ajoute un utilisateur à la collection mealgenius
@@ -104,48 +85,10 @@ public class UserRepository {
 
     }
 
-
-//    public void initDB() {
-//        // Créer une instance du client MongoDB
-//        MongoClientSettings settings = MongoClientSettings.builder()
-//                .applyConnectionString(new ConnectionString("mongodb://localhost:27017"))
-//                .build();
-//        try (MongoClient client = MongoClients.create(settings)) {
-//            // Accéder à la base de données
-//            MongoDatabase database = client.getDatabase(DATABASE_NAME);
-//
-//            // Vérifier si la collection "users" existe déjà
-//            if (!collectionExists(database, "users")) {
-//                // Créer la collection "users"
-//                database.createCollection("users");
-//
-//                // Ajouter un utilisateur au document "mealgenius"
-//                MongoCollection<Document> usersCollection = database.getCollection("users");
-//                Document userDocument = new Document()
-//                        .append("firstName", "John")
-//                        .append("lastName", "Doe")
-//                        .append("email", "johndoe@gmail.com")
-//                        .append("password", "password");
-//                usersCollection.insertOne(userDocument);
-//
-//            }
-//        }
-//    }
-
     public MongoDatabase getConnexion(){
         MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
         MongoDatabase database = mongoClient.getDatabase("mealgenius");
         MongoCollection<Document> collection = database.getCollection("users");
         return database;
     }
-
-
-//private boolean collectionExists(MongoDatabase database, String collectionName) {
-//        for (String name : database.listCollectionNames()) {
-//            if (name.equals(collectionName)) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
 }
