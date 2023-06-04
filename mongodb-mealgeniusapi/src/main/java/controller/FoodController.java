dfb53aadf6859067bf5f43bd47ada97b0897543b
Julien THEME <julien.theme@etu.uca.fr>
2023-06-04 11:24:47 +0200

    💡update comments for FoodController

--- mongodb-mealgeniusapi/src/main/java/controller/FoodController.java
+++ mongodb-mealgeniusapi/src/main/java/controller/FoodController.java
@@ -4,16 +4,15 @@ import dto.FoodDTO;
 import entity.FoodEntity;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import mapper.FoodMapper;
 import repository.FoodRepository;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @Path("/api/foods")
 public class FoodController {
     private final FoodRepository foodRepository;
-
     private FoodMapper foodMapper;
 
     public FoodController(FoodRepository foodRepository) {
@@ -23,6 +22,8 @@ public class FoodController {
 
     @GET
     @Path("/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public List<FoodEntity> getFood() {
         List<FoodEntity> foodEntities = new ArrayList<FoodEntity>();
         foodEntities.addAll(foodRepository.listAll());
@@ -31,6 +32,7 @@ public class FoodController {
 
     @GET
     @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public FoodDTO getFood (@PathParam("id") String id) {
         return FoodMapper.foodToFoodDTO(foodRepository.findById(id));
@@ -39,7 +41,6 @@ public class FoodController {
     @PUT
     @Path("/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public void updateFood (@PathParam("id") String id, FoodDTO food) {
         foodRepository.update(FoodMapper.foodDTOToFood(food));
     }
@@ -47,6 +48,7 @@ public class FoodController {
     @POST
     @Path("/addfood")
     @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response addFood(FoodDTO foodDTO) {
         foodDTO.setId(new ObjectId().toHexString());
         FoodEntity foodEntity = FoodMapper.foodDTOToFood(foodDTO);
@@ -56,6 +58,7 @@ public class FoodController {
 
     @DELETE
     @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
     public void deleteFood(@PathParam("id") String id) {
         FoodEntity foodEntity = foodRepository.findById(id);
         foodRepository.delete(foodEntity);
